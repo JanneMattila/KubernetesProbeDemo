2f546d3c46f135d73681cc26544ff1271666c2cd
Janne Mattila <janne.mattila@hotmail.fi>
2023-06-07 16:16:25 +0300

    Update to use top level statements

--- src/KubernetesProbeDemo/Program.cs
+++ src/KubernetesProbeDemo/Program.cs
@@ -1,19 +1,76 @@
-﻿using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Hosting;
+﻿using KubernetesProbeDemo.Models;
+using KubernetesProbeDemo.Services;
+using Microsoft.OpenApi.Models;
+using System.Reflection;
 
-namespace KubernetesProbeDemo;
+var builder = WebApplication.CreateBuilder(args);
 
-public class Program
+var webhook = builder.Configuration["webhook"];
+var livenessCheck = builder.Configuration.GetValue<bool>("livenessCheck");
+var readinessCheck = builder.Configuration.GetValue<bool>("readinessCheck");
+
+// Following lines can throw if configuration is not correctly set.
+// This correctly prevents container from starting in error scenario.
+// Might throw->
+var healthCheckModel = new HealthCheckModelResponse()
 {
-    public static void Main(string[] args)
-    {
-        CreateHostBuilder(args).Build().Run();
-    }
+    LivenessCheck = livenessCheck,
+    ReadinessCheck = readinessCheck
+};
 
-    public static IHostBuilder CreateHostBuilder(string[] args) =>
-        Host.CreateDefaultBuilder(args)
-            .ConfigureWebHostDefaults(webBuilder =>
-            {
-                webBuilder.UseStartup<Startup>();
-            });
-}
+IHealthCheckRepository healthCheckRepository = new HealthCheckRepository();
+IWebhookHandler webhookHandler = new WebhookHandler(webhook);
+await webhookHandler.InvokeAsync(WebhookEvents.AppStarted, healthCheckRepository.Get());
+// <-Might throw
+
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new OpenApiInfo
+    {
+        Version = "v1",
+        Title = "K8s probe demo API",
+        Description = "Kubernetes liveness and readiness demos",
+        Contact = new OpenApiContact()
+        {
+            Name = "GitHub",
+            Url = new Uri("https://github.com/JanneMattila/KubernetesProbeDemo")
+        },
+        TermsOfService = new Uri("https://github.com/JanneMattila/KubernetesProbeDemo"),
+        License = new OpenApiLicense
+        {
+            Name = "Use under MIT",
+            Url = new Uri("https://opensource.org/licenses/MIT"),
+        }
+    });
+
+    // Set the comments path for the Swagger JSON and UI.
+    var xmlFile = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+    var xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFile);
+    c.IncludeXmlComments(xmlPath);
+});
+
+builder.Services.AddSingleton(webhookHandler);
+builder.Services.AddSingleton(healthCheckRepository);
+
+builder.Services.AddSingleton<IHostedService, BackgroundReportingService>();
+builder.Services.AddMvc();
+
+var app = builder.Build();
+
+app.UseStaticFiles();
+app.UseRouting();
+
+app.UseSwagger(c =>
+{
+    c.SerializeAsV2 = true;
+});
+
+app.UseSwaggerUI(c =>
+{
+    c.SwaggerEndpoint("/swagger/v1/swagger.json", "K8s probe demo API");
+});
+
+app.MapRazorPages();
+app.MapControllers();
+
+app.Run();
--- src/KubernetesProbeDemo/Services/WebhookHandler.cs
+++ src/KubernetesProbeDemo/Services/WebhookHandler.cs
@@ -1,10 +1,7 @@
 ﻿using KubernetesProbeDemo.Models;
-using System;
-using System.Net.Http;
 using System.Net.Mime;
 using System.Text;
 using System.Text.Json;
-using System.Threading.Tasks;
 
 namespace KubernetesProbeDemo.Services;
 
@@ -13,7 +10,7 @@ public class WebhookHandler : IWebhookHandler
     private readonly HttpClient _client = new();
     private readonly bool _enabled;
 
-    public WebhookHandler(string webhookUrl)
+    public WebhookHandler(string? webhookUrl)
     {
         _enabled = !string.IsNullOrEmpty(webhookUrl);
         if (_enabled)
